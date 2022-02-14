83351a2834e7d0de4735906dcf2aa032481039b7
PavelEremeev <pe19950520@yandex.ru>
2022-02-14 12:02:35 +0300

    Добавил функциональность к Select

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -21,8 +21,8 @@ const App = () => {
 	return (
 		<div>
 				<Select data={list}
-				onElementClick={handleElementClick} 
 				currentElementId={currentElementId}
+				setCurrentElementId={setCurrentElementId}
 				/>
 				<Radio data={list} 
 				onElementClick={handleElementClick} 
--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -3,21 +3,28 @@ import React from 'react';
 
 import './Select.css';
 
-const Select = ({ data, onElementClick, currentElementId  }) => {
+
+const Select = ({ data, currentElementId, setCurrentElementId  }) => {
 
 	console.log(currentElementId)
+
+
+	const handleChangeElement = (evt) => {
+		setCurrentElementId(evt.target.value)
+	}
 
 	return (
 		<div className='select'>
 			<p className='select__text'>Наименование фонда:</p>
 			{/* <select 
 			className='select__container'> */}
-			<select value={currentElementId} onChange={() => onElementClick(element)} className='select__container'>
+			<select value={currentElementId}  onChange={handleChangeElement} className='select__container'>
 				{<option  className='select__option'>- - Выберите значение - -</option>}
 				{data.map((element) => <option className='select__option'
 					value={element.entryPointDocument}
-					key={element.entryPointDocument}>{element.description}
-
+					// onClick={() => onElementClick(element)}
+					key={element.entryPointDocument}>
+					{element.description}
 				</option>)}
 			</select>
 		</div>
